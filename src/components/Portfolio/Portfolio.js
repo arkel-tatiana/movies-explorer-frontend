0e373e83434c8a3fe4a1e9eb2246848168aa290d
Arkel Tatiana <tata300-1@yandex.ru>
2022-10-13 16:41:58 +0700

    Дипломная работа.Верстка.Вариант2

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,16 +6,22 @@ function Portfolio() {
         <p className="portfolio__text">Портфолио</p>
         <ul className="portfolio__list">
             <li className="portfolio__list-content">
-                <a href="https://arkel-tatiana.github.io/mesto/" target="_blank" className="portfolio__list-text" rel="noreferrer" >Статичный сайт</a>
-                <img className="portfolio__logo" alt="стрелочка" src={portfolio__logo}/>
+                <a href="https://arkel-tatiana.github.io/mesto/" target="_blank" className="portfolio__list-link" rel="noreferrer" >
+                    <p className="portfolio__list-text">Статичный сайт</p>
+                    <img className="portfolio__logo" alt="стрелочка" src={portfolio__logo}/>
+                </a>
             </li>
             <li className="portfolio__list-content">
-                <a href="https://arkel-tatiana.github.io/russian-travel/" target="_blank" className="portfolio__list-text" rel="noreferrer" >Адаптивный сайт</a>
-                <img className="portfolio__logo" alt="стрелочка" src={portfolio__logo}/>
+                <a href="https://arkel-tatiana.github.io/russian-travel/" target="_blank" className="portfolio__list-link" rel="noreferrer" >
+                    <p className="portfolio__list-text">Адаптивный сайт</p>
+                    <img className="portfolio__logo" alt="стрелочка" src={portfolio__logo}/>
+                </a>
             </li>
             <li className="portfolio__list-content">
-                <a href="https://github.com/arkel-tatiana" target="_blank" className="portfolio__list-text" rel="noreferrer" >Одностраничное приложение</a>
-                <img className="portfolio__logo" alt="стрелочка" src={portfolio__logo}/>
+                <a href="https://github.com/arkel-tatiana" target="_blank" className="portfolio__list-link" rel="noreferrer" >
+                    <p className="portfolio__list-text">Одностраничное приложение</p>
+                    <img className="portfolio__logo" alt="стрелочка" src={portfolio__logo}/>
+                </a>
             </li>
           </ul>
     </div>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,19 +1,34 @@
 import './searchForm.css'
 import logoFind from '../../images/logoFind.svg';
+import React from 'react';
 function SeachForm() {
+  const [searchText, setSearchText] = React.useState('');
+  const [searchError, setSearchError] = React.useState(false);
+  function handleSubmitFound(e) {
+      e.preventDefault();
+      if (searchText.length === 0) {
+          setSearchError(true)
+      } else {
+          setSearchError(false)
+      
+      }
+  } 
   return (
         <div className="seachForm">
-            <div className="seachForm__content">
+            <form className="seachForm__content" onSubmit={handleSubmitFound} noValidate>
                 <input className="seachForm__input"
                     id="found-film"
                     name="foundFilm"
                     type="text"
+                    onChange={event => setSearchText(event.target.value)}
+                    required
                     placeholder="Фильм">
                 </input>
-                <button className="seachForm__button" type="button" aria-label="Поиск фильма" >
+                <span className={`${searchError ? 'seachForm__error' : 'seachForm__error_visible'}`}>Нужно ввести ключевое слово</span>
+                <button className="seachForm__button" type="submit" aria-label="Поиск фильма" >
                     <img className="seachForm__logo" alt="Логотип поиска" src={logoFind}/>
                 </button>
-            </div>
+            </form>
           <div className="seachForm__checkbox-container">
             <input type="checkbox" className="seachForm__checkbox" id="seachForm__checkbox" name="seachForm__checkbox" />
             <label htmlFor="seachForm__checkbox" className="seachForm__label"></label>
